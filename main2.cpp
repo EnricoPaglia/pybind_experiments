c56a5a6cf78a68fdfce88338d98b7f7baabc84ea
Enrico Paglia <enrico.paglia@mail.polimi.it>
2022-03-29 11:45:45 +0200

    functions to synchronize in a separate hpp file

--- main2.cpp
+++ main2.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <random>
-#include <fstream>
-#include <cmath>
-#include <sstream>
-#include <string>
+#include "fun.hpp"
 
 #include <pybind11/embed.h>
 #include <pybind11/pybind11.h>
@@ -12,8 +9,6 @@
 namespace py = pybind11;
 using namespace py::literals;
 
-void synchronize_cpp_to_py_state(const std::mt19937 & cpp_gen, py::object & py_gen, py::module_ & numpy);
-void synchronize_py_to_cpp_state(std::mt19937 & cpp_gen,const py::object & py_gen);
 
 int main(){
 
@@ -43,38 +38,4 @@ int main(){
     return 0;
 }
 
-void synchronize_cpp_to_py_state(const std::mt19937 & cpp_gen, py::object & py_gen, py::module_ & numpy){
 
-    std::stringstream state{};
-    state << cpp_gen;
-
-    std::string auxiliary_string{};
-    py::list state_list{};
-
-    for(unsigned int n = 0; n < 624; ++n){
-        state >> auxiliary_string;
-        state_list.append(std::stoul(auxiliary_string));
-    }
-    state >> auxiliary_string;
-    unsigned int pos = std::stoul(auxiliary_string);
-
-    py::object array = numpy.attr("array")(state_list,"uint32");
-    py::dict state_dict("key"_a=array,"pos"_a=pos);
-    py::dict d("bit_generator"_a="MT19937","state"_a=state_dict);
-    py_gen.attr("__setstate__")(d);
-
-}
-
-void synchronize_py_to_cpp_state(std::mt19937 & cpp_gen,const py::object & py_gen) {
-
-    py::object py_state = py_gen.attr("__getstate__")();
-    py::object state_ = py_state["state"]["key"].attr("tolist")();
-    auto pos_ = py_state["state"]["pos"].cast<unsigned int>();
-
-    std::stringstream ss_state_;
-    for(auto elem:state_){
-        ss_state_ << elem.cast<unsigned int>() << " ";
-    }
-    ss_state_ << pos_;
-    ss_state_ >> cpp_gen;
-}
