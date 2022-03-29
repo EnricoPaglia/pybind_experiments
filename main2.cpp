3277da0a0a7bc0c81dfc8efc949b169e4bc4a948
Enrico Paglia <enrico.paglia@mail.polimi.it>
2022-03-29 11:37:17 +0200

    Improve functions

--- main2.cpp
+++ main2.cpp
@@ -21,14 +21,7 @@ int main(){
 
     // Create a generator with seed 10 and save it in a std::stringstream object
     std::mt19937 gen(10);
-    std::stringstream state{};
-    state << gen;
-    //std::cout << state.str() << std::endl;
-
-    std::mt19937 gen_new(0);
-
-    //state >> gen_new;
-    //std::cout << (gen == gen_new) << std::endl;
+    std::mt19937 gen2(0);
 
     // Create a generator in python
     auto numpy = py::module_::import("numpy");
@@ -36,46 +29,50 @@ int main(){
     auto py_engine = numpy_random.attr("MT19937")();
     auto py_gen = numpy_random.attr("Generator")(py_engine);
 
-//    synchronize_cpp_to_py_state(gen,py_gen,numpy);
-//    std::cout << gen << std::endl;
-//    py::print(py_gen.attr("__getstate__")());
+    synchronize_cpp_to_py_state(gen,py_gen,numpy);
+    synchronize_py_to_cpp_state(gen2,py_gen);
 
-    synchronize_py_to_cpp_state(gen,py_gen);
-    std::cout << gen << std::endl;
-    py::print(py_gen.attr("__getstate__")());
+    if(gen==gen2){
+        std::cout << "The states are the same" << std::endl;
+    } else{
+        std::cout << "The states are different" << std::endl;
+    }
 
+    return 0;
 }
 
 void synchronize_cpp_to_py_state(const std::mt19937 & cpp_gen, py::object & py_gen, py::module_ & numpy){
 
     std::stringstream state{};
     state << cpp_gen;
-    std::string s{};
+
+    std::string auxiliary_string{};
     py::list state_list{};
-//    needed ?
-//    state.clear();
-//    state.seekg(0);
+
     for(unsigned int n = 0; n < 624; ++n){
-        state >> s;
-        state_list.append(std::stoul(s));
+        state >> auxiliary_string;
+        state_list.append(std::stoul(auxiliary_string));
     }
-    auto array = numpy.attr("array")(state_list,"uint32");
-    state >> s;
-    unsigned int pos = std::stoul(s);
+    state >> auxiliary_string;
+    unsigned int pos = std::stoul(auxiliary_string);
+
+    py::object array = numpy.attr("array")(state_list,"uint32");
     py::dict state_dict("key"_a=array,"pos"_a=pos);
     py::dict d("bit_generator"_a="MT19937","state"_a=state_dict);
     py_gen.attr("__setstate__")(d);
+
 }
 
 void synchronize_py_to_cpp_state(std::mt19937 & cpp_gen,const py::object & py_gen) {
-    auto py_state = py_gen.attr("__getstate__")();
-    auto state_ = py_state["state"]["key"];
-    auto pos_ = py_state["state"]["pos"];
-    auto list_state_ = state_.attr("tolist")();
+
+    py::object py_state = py_gen.attr("__getstate__")();
+    py::object state_ = py_state["state"]["key"].attr("tolist")();
+    auto pos_ = py_state["state"]["pos"].cast<unsigned int>();
+
     std::stringstream ss_state_;
-    for(auto elem:list_state_){
+    for(auto elem:state_){
         ss_state_ << elem.cast<unsigned int>() << " ";
     }
-    ss_state_ << pos_.cast<unsigned int>();
+    ss_state_ << pos_;
     ss_state_ >> cpp_gen;
 }
