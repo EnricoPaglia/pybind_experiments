1cc41fdc7ab9cfb6d15092aa207523b1124623a3
Enrico Paglia <enrico.paglia@mail.polimi.it>
2022-03-29 11:39:33 +0200

    comments

--- main2.cpp
+++ main2.cpp
@@ -19,7 +19,7 @@ int main(){
 
     py::scoped_interpreter guard{};
 
-    // Create a generator with seed 10 and save it in a std::stringstream object
+    // Create 2 generators with different seeds
     std::mt19937 gen(10);
     std::mt19937 gen2(0);
 
@@ -29,11 +29,13 @@ int main(){
     auto py_engine = numpy_random.attr("MT19937")();
     auto py_gen = numpy_random.attr("Generator")(py_engine);
 
+    // Synchronize py_gen with gen and then gen2 with py_gen
     synchronize_cpp_to_py_state(gen,py_gen,numpy);
     synchronize_py_to_cpp_state(gen2,py_gen);
 
+    // Check if the states are equal
     if(gen==gen2){
-        std::cout << "The states are the same" << std::endl;
+        std::cout << "The states are equal" << std::endl;
     } else{
         std::cout << "The states are different" << std::endl;
     }
